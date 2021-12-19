6de4ecd100f7be9b6f631ca8c09b452a2986966d
zhj <zhaohaijin@seassoon.com>
2021-12-19 18:43:42 +0800

    local cache

---
+++ localcache/src/test/java/com/shine/localcache/LocalcacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shine.localcache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalcacheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
